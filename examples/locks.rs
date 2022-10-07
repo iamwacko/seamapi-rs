d7d5271594f49c4c46007fcfa1c2d408e73682e7
iamwacko <iamawacko@protonmail.com>
2022-10-07 16:06:19 -0700

    fixed typo

--- examples/locks.rs
+++ examples/locks.rs
@@ -1,7 +1,7 @@
 use seamapi_rs::Seam;
 
 fn main() {
-    let seam = Seam(None, None).unwrap();
+    let seam = Seam::new(None, None).unwrap();
 
     let all_locks = seam.locks().list().unwrap();
     let some_lock = all_locks[1];
